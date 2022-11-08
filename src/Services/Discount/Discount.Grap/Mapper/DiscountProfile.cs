2a7089c682d39334b9b2feb3262669e7a8580966
Jorman Garcia <jagarcia@lfs-inc.com>
2022-11-07 21:03:19 -0500

    Add api Grcp Discount, Entities, respository, interface, protos, service and mapper

---
+++ src/Services/Discount/Discount.Grap/Mapper/DiscountProfile.cs
@@ -0,0 +1,14 @@
+﻿using Discount.Grcp.Entities;
+using Discount.Grcp.Protos;
+using AutoMapper;
+
+namespace Discount.Grcp.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grap/Repositories/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grcp.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.Grcp.Repositories
+{
+    public interface IDiscountRepository
+    {
+
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
