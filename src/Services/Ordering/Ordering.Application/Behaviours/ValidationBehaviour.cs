4587d825e66f92af4197a26d1f698ccd533fd6b0
Jorman Garcia <jagarcia@lfs-inc.com>
2022-11-26 13:44:17 -0500

    Add MicroService service OrderService, Add Application, add Domain, add Infrastructure, with clean arquitecture

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using FluentValidation.Validators;
+using MediatR;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : MediatR.IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validator;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validator)
+        {
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validator.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(_validator.Select(x => x.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(c => c != null).ToList();
+
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
