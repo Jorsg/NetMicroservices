601490f818fa053b968fdeca3c931a798db4b76a
Jorman Garcia <jagarcia@lfs-inc.com>
2022-12-12 15:08:24 -0500

    Ordering.API layer Infraestructure. Add implement Repository Base and repository. Clean arqchitecture, add registration services on the project

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+    {
+        protected readonly OrderContext _dbContex;
+
+        public RepositoryBase(OrderContext dbContex)
+        {
+            _dbContex = dbContex ?? throw new ArgumentNullException(nameof(dbContex));
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync() 
+        {
+            return await _dbContex.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContex.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluString = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContex.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(incluString)) query = query.Include(incluString);
+
+            if(predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContex.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContex.Set<T>().Add(entity);
+            await _dbContex.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContex.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (includes != null) query = includes.Aggregate(query, (current, include) => current.Include(include));
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContex.Entry(entity).State = EntityState.Modified;
+            await _dbContex.SaveChangesAsync();
+        }
+
+        public async Task DeleeteAsync(T entity)
+        {
+            _dbContex.Set<T>().Remove(entity);
+            await _dbContex.SaveChangesAsync();
+        }
+    }
+}
